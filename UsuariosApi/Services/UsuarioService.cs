872a1e25dddc676e89bb7d16655e38e9b896e131
J3eff <jeffersonvideo125@gmail.com>
2023-10-12 16:15:00 -0300

    Gerando o token

--- UsuariosApi/Services/UsuarioService.cs
+++ UsuariosApi/Services/UsuarioService.cs
@@ -10,12 +10,14 @@ namespace UsuariosApi.Services
         private IMapper _mapper;
         private UserManager<Usuario> _userManager;
         private SignInManager<Usuario> _signInManager;
+        private TokenService _tokenService;
 
-        public UsuarioService(UserManager<Usuario> userManager, IMapper mapper, SignInManager<Usuario> signInManager)
+        public UsuarioService(UserManager<Usuario> userManager, IMapper mapper, SignInManager<Usuario> signInManager, TokenService tokenService)
         {
             _userManager = userManager;
             _mapper = mapper;
             _signInManager = signInManager;
+            _tokenService = tokenService;
         }
 
         public async Task Cadastro(CreateUsuarioDto dto)
@@ -23,19 +25,22 @@ namespace UsuariosApi.Services
             Usuario usuario = _mapper.Map<Usuario>(dto);
             IdentityResult result = await _userManager.CreateAsync(usuario, dto.Password);
 
-            if (result.Succeeded) return;
+            if (!result.Succeeded)
+                throw new ApplicationException("Falha ao cadastrar usuário!");
 
-            throw new ApplicationException("Falha ao cadastrar usuário!");
+            return;
         }
 
         public async Task Login(LoginUsuarioDto dto)
         {
             var resultado = await _signInManager.PasswordSignInAsync(dto.Username, dto.Password, false, false);
 
-            if (resultado.Succeeded) return;
+            if (!resultado.Succeeded) 
+                throw new ApplicationException("Usuário não autenticado!");
 
-            throw new ApplicationException("Usuário não autenticado!");
+            _tokenService.GenerateToken(usuario);
 
+            return;
         }
     }
 }
